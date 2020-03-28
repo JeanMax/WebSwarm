432fac0940467a42c494372bcdb096bd36c84f30
JeanMax <mc@rmrf.fr>
2020-03-28 14:36:03 +0100

    Add cookie login

---
+++ src/WebShell/templates/js/cookie.js
@@ -0,0 +1,28 @@
+'use strict';
+
+function get_cookie(key) {
+    var cookie, ret = null;
+    document.cookie.split(';').forEach(c => {
+        cookie = c.split("=");
+        if (cookie[0].trim() == key) {
+            ret = cookie[1];
+        }
+    });
+    return ret;
+}
+
+function set_cookie(key, value, days) {
+    if (typeof(days) === 'undefined') {
+        days = 365;
+    }
+    var date = new Date();
+    date.setTime(date.getTime() + (days * 24 * 3600 * 1000));
+
+    document.cookie = key + "=" + value + "; "
+    + "expires=" + date.toGMTString() + "; "
+    + "path=/";
+}
+
+function del_cookie(key) {
+    set_cookie(key, "", -1);
+}
--- src/WebShell/templates/js/main.js
+++ src/WebShell/templates/js/main.js
@@ -1,6 +1,27 @@
-var root = document.getElementById("root");
+'use strict';
 
-m.render(
-  root,
-  <h2>Mithril? yep</h2>
-);
+document.addEventListener('DOMContentLoaded', function () {
+    var root = document.getElementById("root");
+
+    var Title = {
+        view: function (vnode) {
+            return <h2>{vnode.children}</h2>
+        }
+    };
+
+    m.render(
+        root,
+        <Title>Mithril? yes</Title>
+    );
+});
+
+
+function login() {
+    var username = document.getElementById("login-input").value;
+    set_cookie("user", username);
+    document.getElementById("login-modal").classList.remove("is-active");
+}
+
+function logout() {
+    del_cookie("user");
+}
