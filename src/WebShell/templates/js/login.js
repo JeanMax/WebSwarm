75b576766376db5700442dd37ca93d870501dd56
JeanMax <mc@rmrf.fr>
2020-03-29 15:54:35 +0200

    Validate username

--- src/WebShell/templates/js/login.js
+++ src/WebShell/templates/js/login.js
@@ -9,12 +9,34 @@ function open_modal() {
 
 function LoginModal(initialVnode) {
 
+    function is_valid_username(s) {
+        return /[a-zA-Z0-9_-]{2,}/.test(s);
+    }
+
+    function validate_username() {
+        var username_value = document.getElementById("login-input").value;
+        if (!username_value) {
+            document.getElementById("login-input").classList.remove("is-success");
+            document.getElementById("login-input").classList.remove("is-danger");
+            document.getElementById("login-helper").textContent = ""
+        } else if (is_valid_username(username_value)) {
+            document.getElementById("login-input").classList.remove("is-danger");
+            document.getElementById("login-input").classList.add("is-success");
+            document.getElementById("login-helper").textContent = ""
+        } else {
+            document.getElementById("login-input").classList.remove("is-success");
+            document.getElementById("login-input").classList.add("is-danger");
+            document.getElementById("login-helper").textContent = "Invalid Username"
+        }
+    }
+
     function login() {
         var username_value = document.getElementById("login-input").value;
-        //TODO: input validation
-        set_cookie("user", username_value);
-        g_username = username_value;
-        close_modal();
+        if (is_valid_username(username_value)) {
+            set_cookie("user", username_value);
+            g_username = username_value;
+            close_modal();
+        }
     }
 
     function close_modal() {
@@ -26,8 +48,11 @@ function LoginModal(initialVnode) {
         var e = event || window.event;
         if (e.keyCode === 27) {
             close_modal();
+        } else if (e.keyCode == 13 && document.getElementById("login-modal")
+                                              .classList.value.includes("is-active")) {
+            login();
+            m.redraw();
         }
-        //TODO: enter to send form
     });
 
     return {
@@ -35,22 +60,23 @@ function LoginModal(initialVnode) {
             return (
                 <div class="modal" id="login-modal">
                   <div class="modal-background" onclick={close_modal}></div>
+                    <div class="modal-content is-clipped">
+                      <div class="columns is-mobile is-centered">
+                        <div class="column is-half">
 
-                  <div class="modal-content is-clipped">
-                    <div class="columns is-mobile is-centered">
-                      <div class="column is-half">
-                        <div class="field has-addons">
-                          <div class="control">
-                            <input class="input" id="login-input" type="text" placeholder="Enter your name"/>
+                          <div class="field has-addons">
+                            <div class="control">
+                              <input class="input" id="login-input" type="text" placeholder="Enter your name" onkeyup={validate_username}/>
+                            </div>
+                            <div class="control">
+                              <a class="button is-info" onclick={login}>Log in</a>
+                            </div>
                           </div>
-                          <div class="control">
-                            <a class="button is-info" onclick={login}>Log in</a>
-                          </div>
+                          <p class="help is-danger" id="login-helper"></p>
+
                         </div>
                       </div>
                     </div>
-                  </div>
-
                   <button class="modal-close is-large" aria-label="close" onclick={close_modal}></button>
                 </div>
             );
