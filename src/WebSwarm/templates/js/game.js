79bd438a32f931cd5db44fb5aa22b355ef661003
JeanMax <mc@rmrf.fr>
2020-04-10 00:20:31 +0200

    Refactor game.js

--- src/WebSwarm/templates/js/game.js
+++ src/WebSwarm/templates/js/game.js
@@ -3,10 +3,40 @@
  * https://github.com/kiki727/geMithril
  */
 var g_is_running = false;
+var g_show_fps = true;
 
 
-function Unit() {
-    let vector = null;
+function Fps() {
+    let fps = 0;
+    const times = [];
+
+    function count_fps(){
+        const now = performance.now();
+        while (times.length > 0 && times[0] <= now - 1000) {
+            times.shift();
+        }
+        times.push(now);
+        fps = times.length;
+    }
+
+    return {
+        onupdate: function() {
+            if (g_is_running && g_show_fps) {
+                count_fps();
+            }
+        },
+
+        view: function() {
+            return (
+                <p class="content" id="fps">{g_show_fps ? fps + " fps" : ""}</p>
+            );
+        }
+    };
+}
+
+
+function Boid() {
+    let boid = null;
 
     function kill(event) {
         if (!g_is_running) {
@@ -19,7 +49,7 @@ function Unit() {
         target.style.backgroundSize = "";
         target.classList.add("explosion");
         setTimeout(() => {
-            vector.is_alive = false;
+            boid.is_alive = false;
             target.classList.remove("explosion");
             target.classList.add("dead");
         }, 500);
@@ -27,18 +57,18 @@ function Unit() {
 
     return {
         oninit: function(vnode) {
-            vector = vnode.attrs.vector;
-            vector.is_alive = true;
+            boid = vnode.attrs.boid;
+            boid.is_alive = true;
         },
 
         view: function() {
             return (
                 <div class="unit"
                      onclick={(e)=>kill(e)}
-                     style={"left:" + vector.x + "%;" +
-                            "top:" + vector.y + "%;" +
-                            "width:" + vector.w + "%;" +
-                            "height:" + vector.h + "%"}>
+                     style={"left:" + boid.x + "%;" +
+                            "top:" + boid.y + "%;" +
+                            "width:" + boid.w + "%;" +
+                            "height:" + boid.h + "%"}>
                 </div>
             );
         }
@@ -46,88 +76,41 @@ function Unit() {
 }
 
 
-function create_vectors(n) {
-    const vectors = [];
-    for (let i = 0; i < n; i++) {
-        vectors.push(RandomVector());
-    }
-    return vectors;
-}
-
-
-function Game() {
-    let show_fps = false;
-    let fps = 0;
-    const times = [];
-    // const framerate = 30;
-
-    const max_unit = 100;
-    const vectors = create_vectors(max_unit);
-
-    function units_to_html() {
-        return vectors.map(v=> {
-            return <Unit vector={v} key={v.key}/>;
-        });
-    }
-
-    function start() {
-        console.log("start!");
-        g_is_running = true;  //setInterval(m.redraw, 1000 / framerate);
-    }
-
-    function stop() {
-        console.log("stop!");
-        // clearInterval(g_is_running);
-        g_is_running = false;
-    }
-
-    function count_fps(){
-        const now = performance.now();
-        while (times.length > 0 && times[0] <= now - 1000) {
-            times.shift();
-        }
-        times.push(now);
-        fps = times.length;
-    }
-
-    // function play_frame() {
-    //     vectors.forEach(v => {
-    //         if (v.is_alive) {
-    //             move(v);
-    //         }
-    //     });
-    // }
+function BoidList() {
+    const max_boids = 100;  //TODO: this should be fetched from server
+    const boids = [];
 
     function server_update(data) {
         if (!g_is_running) {
             return;
         }
-        const update_vectors = JSON.parse(data);
-        for (let i = 0; i < update_vectors.length; i++) {
-            Object.assign(vectors[i], update_vectors[i]);
+        const update_boids = JSON.parse(data);
+        for (let i = 0; i < update_boids.length; i++) {
+            Object.assign(boids[i], update_boids[i]);
         }
         m.redraw();
     }
 
     return {
         oninit: function() {
+            for (let i = 0; i < max_boids; i++) {
+                boids.push(Vector());
+            }
             socket.on("update", server_update);
-            socket.on("test", data => { // DEBUG
-                console.log("test...");
-                console.log(data);
+        },
+
+        view: function() {
+            return boids.map(b => {
+                return <Boid boid={b} key={b.key}/>;
             });
-        },
+        }
+    };
+}
 
-        onupdate: function() {
-            if (!g_is_running) {
-                return;
-            }
-            if (show_fps) {
-                count_fps();
-            }
-            // play_frame();
-        },
 
+function Game() {
+
+    return {
         view: function() {
             return (
                 <div class="box has-text-centered">
@@ -136,24 +119,24 @@ function Game() {
                     <div class="layer" id="layer-background"></div>
 
                     <div class="layer" id="layer-unit">
-                      {units_to_html()}
+                      <BoidList/>
                     </div>
 
                     <div class="layer" id="layer-info">
-                      <p class="content" id="fps">{show_fps ? fps + " fps" : ""}</p>
+                      <Fps/>
                     </div>
                   </div>
 
                   <br />
                   <div class="columns">
                     <div class="column">
-                      <a class="button is-info" onclick={()=> show_fps = !show_fps}>{show_fps ? "Hide" : "Show"} fps</a>
+                      <a class="button is-info" onclick={() => g_show_fps = !g_show_fps}>{g_show_fps ? "Hide" : "Show"} fps</a>
                     </div>
 
                     <div class="column">
                       {g_is_running ?
-                       <a class="button is-warning" onclick={stop}>Stop!</a>
-                       : <a class="button is-success" onclick={start}>Start!</a>
+                       <a class="button is-warning" onclick={() => g_is_running = false}>Stop!</a>
+                       : <a class="button is-success" onclick={() => g_is_running = true}>Start!</a>
                       }
                     </div>
 
